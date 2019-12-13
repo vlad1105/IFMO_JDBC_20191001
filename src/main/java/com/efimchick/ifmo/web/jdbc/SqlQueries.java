3b75d6b16a36cb5788baca1c87de69f544ac0075
Vlad <vlad11-samarin@yandex.ru>
2019-12-13 10:01:57 +0300

    First commit

--- src/main/java/com/efimchick/ifmo/web/jdbc/SqlQueries.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SqlQueries.java
@@ -6,49 +6,49 @@ package com.efimchick.ifmo.web.jdbc;
 public class SqlQueries {
     //Select all employees sorted by last name in ascending order
     //language=HSQLDB
-    String select01 = null;
+    String select01 = "SELECT * FROM EMPLOYEE ORDER BY LASTNAME ASC";
 
     //Select employees having no more than 5 characters in last name sorted by last name in ascending order
     //language=HSQLDB
-    String select02 = null;
+    String select02 = "SELECT * FROM EMPLOYEE WHERE LENGTH(LASTNAME) <= 5 ORDER BY LASTNAME ASC";
 
     //Select employees having salary no less than 2000 and no more than 3000
     //language=HSQLDB
-    String select03 = null;
+    String select03 = "SELECT * FROM EMPLOYEE WHERE SALARY >= 2000 AND SALARY <= 3000";
 
     //Select employees having salary no more than 2000 or no less than 3000
     //language=HSQLDB
-    String select04 = null;
+    String select04 = "SELECT * FROM EMPLOYEE WHERE SALARY <= 2000 OR SALARY >= 3000";
 
     //Select employees assigned to a department and corresponding department name
     //language=HSQLDB
-    String select05 = null;
+    String select05 = "SELECT E.ID, FIRSTNAME, LASTNAME, MIDDLENAME, POSITION, MANAGER, HIREDATE, SALARY, DEPARTMENT, NAME FROM EMPLOYEE E INNER JOIN DEPARTMENT D on DEPARTMENT = D.ID";
 
     //Select all employees and corresponding department name if there is one.
     //Name column containing name of the department "depname".
     //language=HSQLDB
-    String select06 = null;
+    String select06 = "SELECT E.ID, FIRSTNAME, LASTNAME, MIDDLENAME, POSITION, MANAGER, HIREDATE, SALARY, DEPARTMENT, NAME AS depname FROM EMPLOYEE E LEFT JOIN DEPARTMENT D on DEPARTMENT = D.ID";
 
     //Select total salary pf all employees. Name it "total".
     //language=HSQLDB
-    String select07 = null;
+    String select07 = "SELECT SUM(SALARY) AS total FROM EMPLOYEE";
 
     //Select all departments and amount of employees assigned per department
     //Name column containing name of the department "depname".
     //Name column containing employee amount "staff_size".
     //language=HSQLDB
-    String select08 = null;
+    String select08 = "SELECT NAME AS depname, COUNT(E.DEPARTMENT) AS staff_size FROM DEPARTMENT JOIN EMPLOYEE E on DEPARTMENT.ID = E.DEPARTMENT GROUP BY DEPARTMENT.NAME";
 
     //Select all departments and values of total and average salary per department
     //Name column containing name of the department "depname".
     //language=HSQLDB
-    String select09 = null;
+    String select09 = "SELECT D.NAME AS depname, SUM(E.SALARY) as total, AVG(E.SALARY) as average FROM DEPARTMENT D, EMPLOYEE E WHERE D.ID = E.DEPARTMENT GROUP BY D.NAME";
 
     //Select all employees and their managers if there is one.
     //Name column containing employee lastname "employee".
     //Name column containing manager lastname "manager".
     //language=HSQLDB
-    String select10 = null;
+    String select10 = "SELECT e1.LASTNAME AS employee, e2.LASTNAME AS manager FROM EMPLOYEE e1 LEFT JOIN EMPLOYEE e2 ON e1.MANAGER = e2.ID";
 
 
 }
